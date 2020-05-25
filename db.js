44a694a36778b9b31fe183627e44c7b09b039b4b
ashrika <kadianashrika@gmail.com>
2020-05-25 20:44:37 +0530

    hello world heroku server

--- db.js
+++ db.js
@@ -1,27 +1,36 @@
 const Sequelize = require('sequelize')
-const db = new Sequelize({
-    dialect:'sqlite',
-    storage:__dirname + '/test.db'
+
+let db;
+
+//if (process.env.DATABASE_URL) {
+ // db = new Sequelize(process.env.DATABASE_URL)
+// } else {
+  db = new Sequelize({
+    dialect: 'sqlite',
+    storage: __dirname + '/test.db'
+  })
+//}
+
+const Tasks = db.define('task', {
+  id: {
+    type: Sequelize.DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false
+  },
+  done: {
+    type: Sequelize.DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  priority: {
+    type: Sequelize.DataTypes.ENUM('high', 'low', 'normal'),
+    defaultValue: 'normal'
+  }
 })
-const Tasks = db.define('task',{
-     id:{
-         type:Sequelize.DataTypes.INTEGER,
-         primaryKey:true,
-         autoIncrement:true
-     },
-     name:{
-         type:Sequelize.DataTypes.STRING,
-         allowNull:false
-     },
-     done:{
-         type:Sequelize.DataTypes.BOOLEAN,
-         defaultValue:false
-     },
-     priority:{
-         type:Sequelize.DataTypes.ENUM('high','low','normal'),
-         defaultValue:'normal'
-     }
-})
-module.exports={
-    db,Tasks
+
+module.exports = {
+  db, Tasks
 }
