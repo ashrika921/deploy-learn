91f631c64f025d36d65f809d37a42774691bbaba
ashrika <kadianashrika@gmail.com>
2020-05-25 18:26:57 +0530

    hello world heroku server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const PORT= process.env.PORT || 4444  
 app.get('/',(req,res)=>{
     res.send("hello world")
 })
-app.listen(4444,(req,res)=>{
-    console.log('server started on http://localhost:4444')
+app.listen(PORT,(req,res)=>{
+    console.log('server started on http://localhost:${PORT}')
 })
