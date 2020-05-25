d15b15f5f829fe0be3aa79cd674cccaac8c3cce8
ashrika <kadianashrika@gmail.com>
2020-05-25 19:56:07 +0530

    hello world heroku server

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const PORT= process.env.PORT || 4444
 app.get('/',(req,res)=>{
     res.send("hello world")
 })
-app.post('/tasks',async (req,res)=>{
-    res.send(await Tasks.create(req.body))
-})
 app.get('/tasks',async (req,res)=>{
     res.send(await Tasks.findAll())
 })
-app.listen(PORT,(req,res)=>{
-    console.log('server started on http://localhost:${PORT}')
+app.post('/tasks',async (req,res)=>{
+    res.send(await Tasks.create(req.body))
+})
+db.sync({alter:true}).then(() => {
+    app.listen(POST,(req,res)=>{
+        console.log('server started on http://localhost:${PORT}')
+    })
 })
